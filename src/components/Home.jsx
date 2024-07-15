c6ee22ce3f4773ef2e02b7b3854768e90cc50556
ravin <ravinchakrawarti@gmail.com>
2024-07-15 13:19:25 +0530

    Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   console.log(data);
   return (
     <div className="flex mt-20">
-      <Sidebar />
+      <Sidebar /> <div></div>
       <div className="h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
         <ListItems />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
